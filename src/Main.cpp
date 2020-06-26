cd4b723a5176fb86d81ca6d94f9f08cb036e8997
Eddie Hersh <eddie_hersh@yahoo.com>
2020-06-25 17:54:44 -0700

    Tested a few more sfml events and added new background images

--- src/Main.cpp
+++ src/Main.cpp
@@ -20,7 +20,6 @@ int main()
 	int curArea = rect.area();
 	cout << "area: " << curArea << endl
 		 << endl;
-
 #endif
 
 	sf::RenderWindow window;
@@ -35,7 +34,9 @@ int main()
 	shape.setFillColor(sf::Color::White);
 
 	sf::Texture shapeTexture;
-	if (!shapeTexture.loadFromFile("content/sfml.png"))
+	//if (!shapeTexture.loadFromFile("content/sfml.png"))
+	//if (!shapeTexture.loadFromFile("content/star-wars-backgrounds-25.jpg"))
+	if (!shapeTexture.loadFromFile("content/DarkSkull.jpg"))
 		return EXIT_FAILURE;
 	shape.setTexture(&shapeTexture);
 
@@ -52,29 +53,56 @@ int main()
 			// check the type of the event...
 			switch (event.type)
 			{
-				// Close window: exit
-				case sf::Event::Closed:
-					window.close();
-					break;
+			// Close window: exit
+			case sf::Event::Closed:
+				window.close();
+				break;
 
-				// Window is resized
-				case sf::Event::Resized:
+			// Window is resized
+			case sf::Event::Resized:
 #if defined(_DEBUG)
-					cout << "new width: " << event.size.width << endl;
-					cout << "new height: " << event.size.height << endl;
+				cout << "new width: " << event.size.width << endl;
+				cout << "new height: " << event.size.height << endl;
 #endif
-					break;
+				break;
 
-				// key pressed
-				case sf::Event::KeyPressed:
+			// key pressed
+			case sf::Event::KeyPressed:
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+				{
+					// move left...
+				}
+				else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+				{
+					// move right...
+				}
+				else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+				{
+					// quit...
+				}
 
-					break;
+				break;
 
-				// we don't process other types of events
-				default:
-					break;
+			// Mouse button pressed
+			case sf::Event::MouseButtonPressed:
+				if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+				{
+					// left mouse button is pressed: shoot
+					//gun.fire();
+
+#if defined(_DEBUG)
+					// get window mouse position
+					sf::Vector2i position = sf::Mouse::getPosition(window);
+					cout << "Mouse position: (" << position.x << ", " << position.y << ")" << endl;
+#endif
+				}
+				break;
+
+			// we don't process other types of events
+			default:
+				break;
 			} // end switch (event.type)
-		}
+		}	  //while (window.pollEvent(event))
 
 		// Clear screen
 		window.clear();
@@ -85,10 +113,10 @@ int main()
 		// Update the window
 		window.display();
 
-	} // // End the game loop
+	} // End the game loop
 
-	sf::Time elapsed = clock.getElapsedTime();
 #if defined(_DEBUG)
+	sf::Time elapsed = clock.getElapsedTime();
 	cout << "Elapsed time (in seconds): " << elapsed.asSeconds() << endl;
 #endif
 
